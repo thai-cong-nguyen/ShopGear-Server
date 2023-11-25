eebe3f69d1896183baef1858e5ec048212e2e9a9
uKaxg <uka.pgr@gmail.com>
2023-11-25 16:14:24 +0700

    resolve conflicts

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
-from . import views
+from .views import crud
+from rest_framework import routers
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('users/', views.user_list, name='all users'),
-    path('users/<int:id>', views.user_detail, name='a detail user')
+    path('', crud.index),
+    path('users/', crud.user_list, name='all users'),
+    path('users/<int:pk>', crud.user_detail, name='a detail user'),
+    path('categories/', crud.category_list, name='all categories'),
+    path('categories/<int:pk>', crud.category_detail, name='a detail category'),
+    path('products/', crud.ProductList.as_view(), name='all products'),
+    path('products/<int:pk>', crud.ProductDetail.as_view(),
+         name='a detail product'),
 ]
---
+++ store/views/crud.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render
+from ..models import Category, User, Product, Order, OrderItem, Cart, CartItem, Transaction, Post
+
+from ..serializers import UserSerializer, CategorySerializer, ProductSerializer, OrderSerializer, OrderItemSerializer, CartSerializer, CartItemSerializer, TransactionSerializer, PostSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status, mixins, generics
+
+
+def index(request):
+    obj = Category.objects.all()
+    context = {
+        'obj': obj
+    }
+    return render(request, 'index.html', context)
+
+# USER REQUEST
+
+
+@api_view(['GET', 'POST'])
+def user_list(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# CATEGORY REQUEST
+
+
+@api_view(['GET', 'POST'])
+def category_list(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def category_detail(request, id):
+    try:
+        category = Category.objects.get(pk=id)
+    except Category.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# PRODUCT REQUESTS
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
