2f25100899a8a5b09ccb9f9b14dbcfb4875e7b79
uKaxg <uka.pgr@gmail.com>
2023-12-23 18:19:33 +0700

    change some api and its name

--- store/urls.py
+++ store/urls.py
@@ -3,11 +3,11 @@ from .views import crud
 from rest_framework import routers
 urlpatterns = [
     path('', crud.ApiRoot.as_view(), name='api-root'),
-    path('users/', crud.user_list, name='user-list'),
-    path('users/<int:pk>/', crud.user_detail, name='user-detail'),
-    path('categories/', crud.category_list, name='category-list'),
-    path('categories/<int:pk>/', crud.category_detail, name='category-detail'),
-    path('products/', crud.ProductList.as_view(), name='product-list'),
-    path('products/<int:pk>/', crud.ProductDetail.as_view(),
+    path('api/users/', crud.user_list, name='user-list'),
+    path('api/users/<int:pk>/', crud.user_detail, name='user-detail'),
+    path('api/categories/', crud.category_list, name='category-list'),
+    path('api/categories/<int:pk>/', crud.category_detail, name='category-detail'),
+    path('api/products/', crud.ProductList.as_view(), name='product-list'),
+    path('api/products/<int:pk>/', crud.ProductDetail.as_view(),
          name='product-detail'),
 ]
