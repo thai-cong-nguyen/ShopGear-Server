28c96452f9dce3210a4fd644424abda913eb26e6
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2024-01-01 15:25:41 +0700

    Adding static folder for fixing issue

--- server/settings.py
+++ server/settings.py
@@ -191,6 +191,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/']  # store css, js files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
