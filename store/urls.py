4fd97af20570eaf17db51bbf142833e5775e4724
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2024-01-01 15:06:29 +0700

    Finished ZaloPay Gateway

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import crud, auth
+from .views import crud, auth, payment
 from rest_framework import routers
 
 from rest_framework_simplejwt.views import (
@@ -8,7 +8,6 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('api/', crud.index),
     path('users/', crud.user_list, name='all users'),
     path('users/<int:pk>', crud.user_detail, name='a detail user'),
     path('categories/', crud.category_list, name='all categories'),
@@ -27,4 +26,7 @@ urlpatterns = [
     path('api/products/', crud.ProductList.as_view(), name='product-list'),
     path('api/products/<int:pk>/', crud.ProductDetail.as_view(),
          name='product-detail'),
+    path('api/payment/create-order', payment.CreateOrderView.as_view(), name='create-payment-order'),
+    path('api/payment/callback', payment.CallbackView.as_view(), name='callback-order'),
+    path('api/payment/query', payment.QueryOrderView.as_view(), name='query-order'),
 ]
